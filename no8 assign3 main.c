4589b1fa9402c9a3f3921c016f5875ed65c1d400
BabuEshwar <130425816+BabuEshwar@users.noreply.github.com>
2023-04-13 13:36:49 +0530

    Update no8 assign3 main.c

--- no8 assign3 main.c
+++ no8 assign3 main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 
 #include <stdio.h>
 #include <string.h> 
@@ -18,9 +10,9 @@ void swap (char *x, char *y)
     *x = *y;
     *y = temp;
 }
-/*  End of swap()  */
+
  
-/*  Function to print permutations of string  */
+
 void permute(char *a, int i, int n)
 {
     int j;
@@ -36,7 +28,7 @@ void permute(char *a, int i, int n)
     }
 }
  
-/*  The main() begins  */
+
 int main()
 {
     char a[20];
