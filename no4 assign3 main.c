21f8c177fc696baa8a9ae2b2f737e15779a69d08
BabuEshwar <130425816+BabuEshwar@users.noreply.github.com>
2023-04-13 13:34:27 +0530

    Update no4 assign3 main.c

--- no4 assign3 main.c
+++ no4 assign3 main.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main() {
@@ -15,7 +8,7 @@ int main() {
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
 
-    // maximum number between n1 and n2 is stored in max
+    
     max = (n1 > n2) ? n1 : n2;
 
     while (1) {
